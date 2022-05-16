ece0a240c7b9387d86046192df7a987ae2f2fbb4
IShahnawazShaikh <shahnawazexplore@gmail.com>
2022-05-16 18:52:47 +0530

    Set up the springboot project

---
+++ 01-java-basic/src/main/java/com/scaler/JavaBasicApplication.java
@@ -0,0 +1,15 @@
+package com.scaler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBasicApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(JavaBasicApplication.class, args);
+        System.out.println("Hello world");
+    }
+
+}
