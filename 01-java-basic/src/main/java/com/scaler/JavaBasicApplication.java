c4c7aefd9959787bef076b7dc59c032034b36d84
IShahnawazShaikh <shahnawazexplore@gmail.com>
2022-05-17 01:06:38 +0530

    packaging jar with other libraries

--- 01-java-basic/src/main/java/com/scaler/JavaBasicApplication.java
+++ 01-java-basic/src/main/java/com/scaler/JavaBasicApplication.java
@@ -1,15 +1,37 @@
 package com.scaler;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class JavaBasicApplication {
+    static OkHttpClient client = new OkHttpClient();
 
     public static void main(String[] args) {
 
         SpringApplication.run(JavaBasicApplication.class, args);
         System.out.println("Hello world");
+        try {
+            getUrl("https://example.com/");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    static String getUrl(String url) throws IOException {
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+
+            try (Response response = client.newCall(request).execute()) {
+                return response.body().string();
+            }
+        }
+
     }
 
-}
