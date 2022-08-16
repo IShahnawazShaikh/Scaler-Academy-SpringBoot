3b0615d4d8d8fbbac17e06774b450f1eb3c6e651
IShahnawazShaikh <shahnawazexplore@gmail.com>
2022-08-16 23:27:47 +0530

    Initialized Project and Added Features with future scope.

---
+++ 04-blogging application/letmeupdate/src/main/java/com/scaler/letmeupdate/LetmeupdateApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.letmeupdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetmeupdateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetmeupdateApplication.class, args);
+    }
+
+}
