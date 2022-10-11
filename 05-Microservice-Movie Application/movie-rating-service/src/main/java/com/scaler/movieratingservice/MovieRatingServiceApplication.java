f7a58ba8671f0ba75a65bfee2db7758bbe8fa6fc
Shahnawaz Shaikh <32239343+IShahnawazShaikh@users.noreply.github.com>
2022-10-11 23:44:03 +0530

    Initialized 3-Spring Boot project for Movie application

---
+++ 05-Microservice-Movie Application/movie-rating-service/src/main/java/com/scaler/movieratingservice/MovieRatingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.movieratingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRatingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieRatingServiceApplication.class, args);
+    }
+
+}
---
+++ 05-Microservice-Movie Application/movie-rating-service/src/test/java/com/scaler/movieratingservice/MovieRatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.movieratingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRatingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
