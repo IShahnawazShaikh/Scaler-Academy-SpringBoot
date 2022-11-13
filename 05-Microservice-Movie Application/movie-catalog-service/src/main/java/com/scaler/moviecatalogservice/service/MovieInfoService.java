c914a5f0142dc82031b28bed1e49c84780425964
Shahnawaz Shaikh <32239343+IShahnawazShaikh@users.noreply.github.com>
2022-11-13 13:25:30 +0530

    Hystrix Parameters Added

--- 05-Microservice-Movie Application/movie-catalog-service/src/main/java/com/scaler/moviecatalogservice/service/MovieInfoService.java
+++ 05-Microservice-Movie Application/movie-catalog-service/src/main/java/com/scaler/moviecatalogservice/service/MovieInfoService.java
@@ -1,6 +1,7 @@
 package com.scaler.moviecatalogservice.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.scaler.moviecatalogservice.models.CatalogItem;
 import com.scaler.moviecatalogservice.models.Movie;
 import com.scaler.moviecatalogservice.models.Rating;
@@ -12,7 +13,13 @@ import org.springframework.web.client.RestTemplate;
 public class MovieInfoService {
     @Autowired
     private RestTemplate restTemplate;
-    @HystrixCommand(fallbackMethod = "getFallbackCatalogItems")
+    @HystrixCommand(fallbackMethod = "getFallbackCatalogItems",
+     commandProperties ={
+             @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+             @HystrixProperty(name="circuitBreaker.requestVolumeThreshold",value = "6"),
+             @HystrixProperty(name="circuitBreaker.errorThresholdPercentage",value = "50"),
+             @HystrixProperty(name="circuitBreaker.sleepWindowInMilliseconds",value = "5000")
+     })
     public CatalogItem getCatalogItems(Rating rating) {
         var movie=restTemplate.getForObject("http://MOVIE-INFO-SERVICE/movie/"+ rating.getMovieId(), Movie.class);
         return new CatalogItem(movie.getMovieName(), movie.getDescription(), rating.getRating());
--- 05-Microservice-Movie Application/movie-catalog-service/src/main/java/com/scaler/moviecatalogservice/service/MovieRatingService.java
+++ 05-Microservice-Movie Application/movie-catalog-service/src/main/java/com/scaler/moviecatalogservice/service/MovieRatingService.java
@@ -1,6 +1,7 @@
 package com.scaler.moviecatalogservice.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.scaler.moviecatalogservice.models.Rating;
 import com.scaler.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,13 @@ import java.util.Arrays;
 public class MovieRatingService {
     @Autowired
     private RestTemplate restTemplate;
-    @HystrixCommand(fallbackMethod = "getFallbackUserRating")
+    @HystrixCommand(fallbackMethod = "getFallbackUserRating",
+            commandProperties ={
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+                    @HystrixProperty(name="circuitBreaker.requestVolumeThreshold",value = "6"),
+                    @HystrixProperty(name="circuitBreaker.errorThresholdPercentage",value = "50"),
+                    @HystrixProperty(name="circuitBreaker.sleepWindowInMilliseconds",value = "5000")
+            })
     public UserRating getUserRating(String userId) {
         return restTemplate.getForObject("http://MOVIE-RATING-SERVICE/rating/users/"+userId, UserRating.class);
     }
